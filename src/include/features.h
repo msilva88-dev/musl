ebf7e781729fb2a5f1454408f11f9b82a99615c0
André Silva <emulatorman@hyperbola.info>
2025-09-10 12:59:39 +0300

    src/include/features.h: make `hidden` expand to empty when MUSL_NO_HIDDEN is set
    Without an alternative definition, headers that use `hidden` produce
    parsing errors (e.g., `hidden int __putenv(...)`) when building CRT
    with -DMUSL_NO_HIDDEN. Define `hidden` to an empty macro in that case.
    
    Signed-off-by: André Silva <emulatorman@hyperbola.info>

--- src/include/features.h
+++ src/include/features.h
@@ -4,7 +4,9 @@
 #include "../../include/features.h"
 
 #define weak __attribute__((__weak__))
-#ifndef MUSL_NO_HIDDEN
+#ifdef MUSL_NO_HIDDEN
+#define hidden /* intentionally empty for CRT builds */
+#else
 #define hidden __attribute__((__visibility__("hidden")))
 #endif
 #define weak_alias(old, new) \
