4dc7090bc92836c346afce07badbc81410e881c5
Marcio Silva <coadde@hyperbola.info>
2025-09-30 16:02:21 -0300

    openpty: adapt var. decl. and ioctl calls for BSD
    - Separate variable declarations for OpenBSD/HyperbolaBSD
      vs Linux to avoid unused variable warnings.
    - Replace Linux-specific TIOCSPTLCK/TIOCGPTN calls
      with PTMGET ioctl for BSD platforms.
    - Copy slave pty name from ptm structure
      and close master slave fd for BSD.
    - No change in Linux behavior;
      purely adds conditional handling for BSD systems.
    
    Signed-off-by: Marcio Silva <coadde@hyperbola.info>

--- src/misc/openpty.c
+++ src/misc/openpty.c
@@ -9,21 +9,40 @@
 
 int openpty(int *pm, int *ps, char *name, const struct termios *tio, const struct winsize *ws)
 {
+#if defined(__OpenBSD__) || defined(__HyperbolaBSD__)
+	struct ptmget ptm;
+	int m, s, cs;
+#elif defined(__linux__)
 	int m, s, n=0, cs;
+#endif
 	char buf[20];
 
+#if defined(__OpenBSD__) || defined(__HyperbolaBSD__)
+	m = open("/dev/ptm", O_RDWR|O_NOCTTY);
+#elif defined(__linux__)
 	m = open("/dev/ptmx", O_RDWR|O_NOCTTY);
+#endif
 	if (m < 0) return -1;
 
 	pthread_setcancelstate(PTHREAD_CANCEL_DISABLE, &cs);
 
+#if defined(__OpenBSD__) || defined(__HyperbolaBSD__)
+	if (ioctl(m, PTMGET, &ptm) < 0)
+#elif defined(__linux__)
 	if (ioctl(m, TIOCSPTLCK, &n) || ioctl (m, TIOCGPTN, &n))
+#endif
 		goto fail;
 
 	if (!name) name = buf;
+#if defined(__OpenBSD__) || defined(__HyperbolaBSD__)
+	strlcpy(name, ptm.sn, sizeof(ptm.sn));
+	s = ptm.cfd;
+	close(ptm.sfd);
+#elif defined(__linux__)
 	snprintf(name, sizeof buf, "/dev/pts/%d", n);
 	if ((s = open(name, O_RDWR|O_NOCTTY)) < 0)
 		goto fail;
+#endif
 
 	if (tio) tcsetattr(s, TCSANOW, tio);
 	if (ws) ioctl(s, TIOCSWINSZ, ws);
