7e41b964a91a1926610533a80b84aeaa0daf79a3
Marcio Silva <coadde@hyperbola.info>
2025-09-30 17:27:43 -0300

    termios/ioctl: implement tcflush for BSD
    - Use TIOCFLUSH ioctl for HyperbolaBSD and OpenBSD.
    - Preserve TCFLSH usage for Linux.
    - Ensures consistent tcflush behavior across BSD and Linux platforms.
    
    Signed-off-by: Marcio Silva <coadde@hyperbola.info>

--- src/termios/tcflush.c
+++ src/termios/tcflush.c
@@ -1,7 +1,14 @@
 #include <termios.h>
 #include <sys/ioctl.h>
+#if defined(__HyperbolaBSD__) || defined(__OpenBSD__)
+#include <errno.h>
+#endif
 
 int tcflush(int fd, int queue)
 {
+#if defined(__HyperbolaBSD__) || defined(__OpenBSD__)
+	return ioctl(fd, TIOCFLUSH, queue);
+#elif defined(__linux__)
 	return ioctl(fd, TCFLSH, queue);
+#endif
 }
