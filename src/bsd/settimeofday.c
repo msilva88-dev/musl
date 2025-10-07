9e18bb52a86f027ae9c63745d6e70e410686a2d2
Marcio Silva <coadde@hyperbola.info>
2025-10-07 16:51:42 -0300

    bsd: implement settimeofday via syscall
    - Replace custom timeval-to-timespec conversion with direct syscall.
    - Uses syscall(SYS_settimeofday) to set time
      and timezone on BSD systems.
    
    Signed-off-by: Marcio Silva <coadde@hyperbola.info>

--- src/bsd/settimeofday.c
+++ src/bsd/settimeofday.c
@@ -4,14 +4,7 @@
 #include <errno.h>
 #include "syscall.h"
 
-int settimeofday(
-	const struct timeval *tv,
-	const struct timezone *tz __attribute__((unused))
-)
+int settimeofday(const struct timeval *tv, const struct timezone *tz)
 {
-	(void)tz;
-	if (!tv) return 0;
-	if (tv->tv_usec >= 1000000ULL) return __syscall_ret(-EINVAL);
-	return clock_settime(CLOCK_REALTIME, &((struct timespec){
-		.tv_sec = tv->tv_sec, .tv_nsec = tv->tv_usec * 1000}));
+	return syscall(SYS_settimeofday, tv, tz);
 }
