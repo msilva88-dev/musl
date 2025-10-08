f388f6c56649b367e9cff7c9e940afb2bce6d9ca
Marcio Silva <coadde@hyperbola.info>
2025-10-08 16:53:37 -0300

    sigsuspend: adjust syscall for BSD vs Linux
    - Use SYS_sigsuspend on HyperbolaBSD/OpenBSD.
    - Use SYS_rt_sigsuspend on Linux with proper sigset size.
    
    Signed-off-by: Marcio Silva <coadde@hyperbola.info>

--- src/signal/sigsuspend.c
+++ src/signal/sigsuspend.c
@@ -3,5 +3,9 @@
 
 int sigsuspend(const sigset_t *mask)
 {
+#if defined(__HyperbolaBSD__) || defined(__OpenBSD__)
+	return syscall_cp(SYS_sigsuspend, mask);
+#elif defined(__linux__)
 	return syscall_cp(SYS_rt_sigsuspend, mask, _NSIG/8);
+#endif
 }
