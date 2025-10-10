ebc52613b15c12eb2b504c87568d521e15722555
Marcio Silva <coadde@hyperbola.info>
2025-10-09 21:38:52 -0300

    unistd: fix getlogin_r syscall on BSD
    - Use `SYS_getlogin_r` instead of `SYS_getlogin`
      for HyperbolaBSD/OpenBSD.
    - Ensures `getlogin_r` behaves correctly on BSD platforms.
    
    Signed-off-by: Marcio Silva <coadde@hyperbola.info>

--- src/unistd/getlogin_r.c
+++ src/unistd/getlogin_r.c
@@ -9,7 +9,7 @@
 int getlogin_r(char *name, size_t size)
 {
 #if defined(__HyperbolaBSD__) || defined(__OpenBSD__)
-	return syscall(SYS_getlogin, name, size);
+	return syscall(SYS_getlogin_r, name, size);
 #elif defined(__linux__)
 	char *logname = getlogin();
 	if (!logname) return ENXIO; /* or...? */
