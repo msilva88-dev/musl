9b3afa44c903b29b4dd5d45efcd247a9ae0ffda8
Marcio Silva <coadde@hyperbola.info>
2025-10-16 20:05:59 -0300

    signal: add BSD impl. for sigtimedwait/sigwait
    - Implement sigtimedwait() and sigwait() using SYS__thrsigdivert
      on HyperbolaBSD and OpenBSD.
    - Exclude SIGTHR from the signal mask to match BSD behavior.
    - Keep Linux path using rt_sigtimedwait and sigtimedwait
      for compatibility.
    
    Signed-off-by: Marcio Silva <coadde@hyperbola.info>

--- src/signal/sigtimedwait.c
+++ src/signal/sigtimedwait.c
@@ -2,11 +2,22 @@
 #include <errno.h>
 #include "syscall.h"
 
+#if defined(__linux__)
 #define IS32BIT(x) !((x)+0x80000000ULL>>32)
 #define CLAMP(x) (int)(IS32BIT(x) ? (x) : 0x7fffffffU+((0ULL+(x))>>63))
+#endif
 
 static int do_sigtimedwait(const sigset_t *restrict mask, siginfo_t *restrict si, const struct timespec *restrict ts)
 {
+#if defined(__HyperbolaBSD__) || defined(__OpenBSD__)
+	struct timespec tsv;
+
+	if (!ts) return __syscall_cp(SYS___thrsigdivert, mask, si, NULL);
+	else {
+		tsv = *ts;
+		return __syscall_cp(SYS___thrsigdivert, mask, si, &tsv);
+	}
+#elif defined(__linux__)
 #ifdef SYS_rt_sigtimedwait_time64
 	time_t s = ts ? ts->tv_sec : 0;
 	long ns = ts ? ts->tv_nsec : 0;
@@ -21,12 +32,20 @@ static int do_sigtimedwait(const sigset_t *restrict mask, siginfo_t *restrict si
 #else
 	return __syscall_cp(SYS_rt_sigtimedwait, mask, si, ts, _NSIG/8);
 #endif
+#endif
 }
 
 int sigtimedwait(const sigset_t *restrict mask, siginfo_t *restrict si, const struct timespec *restrict timeout)
 {
 	int ret;
+#if defined(__HyperbolaBSD__) || defined(__OpenBSD__)
+        sigset_t ssp = *mask;
+
+	sigdelset(&ssp, SIGTHR);
+	do ret = do_sigtimedwait(&ssp, si, timeout);
+#elif defined(__linux__)
 	do ret = do_sigtimedwait(mask, si, timeout);
+#endif
 	while (ret==-EINTR);
 	return __syscall_ret(ret);
 }
--- src/signal/sigwait.c
+++ src/signal/sigwait.c
@@ -1,10 +1,26 @@
 #include <signal.h>
+#include <errno.h>
+#if defined(__HyperbolaBSD__) || defined(__OpenBSD__)
+#include "syscall.h"
+#endif
 
 int sigwait(const sigset_t *restrict mask, int *restrict sig)
 {
 	siginfo_t si;
-	if (sigtimedwait(mask, &si, 0) < 0)
-		return -1;
+#if defined(__HyperbolaBSD__) || defined(__OpenBSD__)
+	sigset_t s = *mask;
+
+	sigdelset(&s, SIGTHR);
+	for (;;) {
+		si.si_signo = syscall_cp(SYS___thrsigdivert, s, NULL, NULL);
+		if (si.si_signo == -1 && errno == EINTR) continue;
+		break;
+	}
+
+	if (si.si_signo < 0) return errno;
+#elif defined(__linux__)
+	if (sigtimedwait(mask, &si, 0) < 0) return errno;
+#endif
 	*sig = si.si_signo;
 	return 0;
 }
