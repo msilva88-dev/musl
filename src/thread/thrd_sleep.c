3f15e3caec33ca8cf7f8ef9e15260818773aedf6
Marcio Silva <coadde@hyperbola.info>
2025-10-20 16:57:53 -0300

    thrd_sleep: adapt to BSD nanosleep syscall
    - Use SYS_nanosleep via __syscall_cp for HyperbolaBSD and OpenBSD.
    - Keep __clock_nanosleep fallback for Linux.
    
    Signed-off-by: Marcio Silva <coadde@hyperbola.info>

--- src/thread/thrd_sleep.c
+++ src/thread/thrd_sleep.c
@@ -5,7 +5,11 @@
 
 int thrd_sleep(const struct timespec *req, struct timespec *rem)
 {
+#if defined(__HyperbolaBSD__) || defined(__OpenBSD__)
+	int ret = -__syscall_cp(SYS_nanosleep, req, rem);
+#elif defined(__linux__)
 	int ret = -__clock_nanosleep(CLOCK_REALTIME, 0, req, rem);
+#endif
 	switch (ret) {
 	case 0:      return 0;
 	case -EINTR: return -1; /* value specified by C11 */
