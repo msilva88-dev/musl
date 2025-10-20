7bba083a86388ebab07146db1704c1e67263812c
Marcio Silva <coadde@hyperbola.info>
2025-10-20 17:01:28 -0300

    exit: use SYS_exit on BSD inst. of SYS_exit_group
    - Call SYS_exit directly on HyperbolaBSD/OpenBSD in _Exit().
    - Keep SYS_exit_group for Linux, preserving behavior.
    
    Signed-off-by: Marcio Silva <coadde@hyperbola.info>

--- src/exit/_Exit.c
+++ src/exit/_Exit.c
@@ -3,6 +3,10 @@
 
 _Noreturn void _Exit(int ec)
 {
+#if defined(__HyperbolaBSD__) || defined(__OpenBSD__)
+	__syscall(SYS_exit, ec);
+#elif defined(__linux__)
 	__syscall(SYS_exit_group, ec);
 	for (;;) __syscall(SYS_exit, ec);
+#endif
 }
