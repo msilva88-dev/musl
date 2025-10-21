1779995aee9b494859e3092b84b34da2bf3ea82e
Marcio Silva <coadde@hyperbola.info>
2025-10-21 18:13:47 -0300

    x32: adjust lseek syscall for BSD
    - Update __lseek for HyperbolaBSD and OpenBSD
      to pass arguments in BSD x32 syscall convention.
    - Moves 'whence' into r10
      and offsets registers according to BSD kernel ABI.
    - Linux path remains unchanged.
    
    Signed-off-by: Marcio Silva <coadde@hyperbola.info>

--- src/unistd/x32/lseek.c
+++ src/unistd/x32/lseek.c
@@ -4,10 +4,18 @@
 off_t __lseek(int fd, off_t offset, int whence)
 {
 	off_t ret;
+#if defined(__HyperbolaBSD__) || defined(__OpenBSD__)
+	register long r10 __asm__("r10") = whence;
+	__asm__ __volatile__ ("syscall"
+		: "=a"(ret)
+		: "a"(SYS_lseek), "D"(fd), "d"(offset), "S"(0), "r"(r10)
+		: "rcx", "r11", "memory");
+#elif defined(__linux__)
 	__asm__ __volatile__ ("syscall"
 		: "=a"(ret)
 		: "a"(SYS_lseek), "D"(fd), "S"(offset), "d"(whence)
 		: "rcx", "r11", "memory");
+#endif
 	return ret < 0 ? __syscall_ret(ret) : ret;
 }
 
