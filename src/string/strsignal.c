7c0db34e3c1a7606a8eb0269524550b4bb66e53a
Marcio Silva <coadde@hyperbola.info>
2025-10-24 17:20:46 -0300

    bsd: extend strsignal mapping for BSD targets
    - Normalize missing signal macros: SIGABRT, SIGCHLD, SIGPOLL.
    - Adjust signal mapping array and descriptions for HyperbolaBSD/OpenBSD.
    - Keep Linux mapping and strings unchanged.
    - Handles optional signals like SIGEMT, SIGWINCH, SIGINFO, SIGTHR gracefully.
    
    Signed-off-by: Marcio Silva <coadde@hyperbola.info>

--- src/string/strsignal.c
+++ src/string/strsignal.c
@@ -2,14 +2,38 @@
 #include <string.h>
 #include "locale_impl.h"
 
-#if (SIGHUP == 1) && (SIGINT == 2) && (SIGQUIT == 3) && (SIGILL == 4) \
+#if !defined(SIGABRT) && defined(SIGIOT)
+#define SIGABRT SIGIOT
+#endif
+
+#if !defined(SIGCHLD) && defined(SIGCLD)
+#define SIGCHLD SIGCLD
+#endif
+
+#if !defined(SIGPOLL) && defined(SIGIO)
+#define SIGPOLL SIGIO
+#endif
+
+#if ((defined(__HyperbolaBSD__) || defined(__OpenBSD__)) \
+ && (SIGHUP == 1) && (SIGINT == 2) && (SIGQUIT == 3) && (SIGILL == 4) \
+ && (SIGTRAP == 5) && (SIGABRT == 6) && ((defined(SIGEMT) && SIGEMT == 7) || !defined(SIGEMT)) && (SIGFPE == 8) \
+ && (SIGKILL == 9) && (SIGBUS == 10) && (SIGSEGV == 11) && (SIGSYS == 12) \
+ && (SIGPIPE == 13) && (SIGALRM == 14) && (SIGTERM == 15) && (SIGURG == 16) \
+ && (SIGSTOP == 17) && (SIGTSTP == 18) && (SIGCONT == 19) && (SIGCHLD == 20) \
+ && (SIGTTIN == 21) && (SIGTTOU == 22) && ((defined(SIGPOLL) && SIGPOLL == 23) || !defined(SIGPOLL)) && (SIGXCPU == 24) \
+ && (SIGXFSZ == 25) && (SIGVTALRM == 26) && (SIGPROF == 27) && ((defined(SIGWINCH) && SIGWINCH == 28) || !defined(SIGWINCH)) \
+ && ((defined(SIGINFO) && SIGINFO == 29) || !defined(SIGINFO)) && (SIGUSR1 == 30) && (SIGUSR2 == 31) \
+ && ((defined(SIGTHR) && SIGTHR == 31) || !defined(SIGTHR)) \
+ ) || (defined(__linux__) \
+ && (SIGHUP == 1) && (SIGINT == 2) && (SIGQUIT == 3) && (SIGILL == 4) \
  && (SIGTRAP == 5) && (SIGABRT == 6) && (SIGBUS == 7) && (SIGFPE == 8) \
  && (SIGKILL == 9) && (SIGUSR1 == 10) && (SIGSEGV == 11) && (SIGUSR2 == 12) \
  && (SIGPIPE == 13) && (SIGALRM == 14) && (SIGTERM == 15) && (SIGSTKFLT == 16) \
  && (SIGCHLD == 17) && (SIGCONT == 18) && (SIGSTOP == 19) && (SIGTSTP == 20) \
  && (SIGTTIN == 21) && (SIGTTOU == 22) && (SIGURG == 23) && (SIGXCPU == 24) \
  && (SIGXFSZ == 25) && (SIGVTALRM == 26) && (SIGPROF == 27) && (SIGWINCH == 28) \
- && (SIGPOLL == 29) && (SIGPWR == 30) && (SIGSYS == 31)
+ && (SIGPOLL == 29) && (SIGPWR == 30) && (SIGSYS == 31) \
+ )
 
 #define sigmap(x) x
 
@@ -22,15 +46,36 @@ static const char map[] = {
 	[SIGILL]    = 4,
 	[SIGTRAP]   = 5,
 	[SIGABRT]   = 6,
+#if defined(__HyperbolaBSD__) || defined(__OpenBSD__)
+#if defined(SIGEMT)
+	[SIGEMT]    = 7,
+#endif
+#elif defined(__linux__)
 	[SIGBUS]    = 7,
+#endif
 	[SIGFPE]    = 8,
 	[SIGKILL]   = 9,
+#if defined(__HyperbolaBSD__) || defined(__OpenBSD__)
+	[SIGBUS]    = 10,
+#elif defined(__linux__)
 	[SIGUSR1]   = 10,
+#endif
 	[SIGSEGV]   = 11,
+#if defined(__HyperbolaBSD__) || defined(__OpenBSD__)
+	[SIGSYS]    = 12,
+#elif defined(__linux__)
 	[SIGUSR2]   = 12,
+#endif
 	[SIGPIPE]   = 13,
 	[SIGALRM]   = 14,
 	[SIGTERM]   = 15,
+#if defined(__HyperbolaBSD__) || defined(__OpenBSD__)
+	[SIGURG]    = 16,
+	[SIGSTOP]   = 17,
+	[SIGTSTP]   = 18,
+	[SIGCONT]   = 19,
+	[SIGCHLD]   = 20,
+#elif defined(__linux__)
 #if defined(SIGSTKFLT)
 	[SIGSTKFLT] = 16,
 #elif defined(SIGEMT)
@@ -40,17 +85,39 @@ static const char map[] = {
 	[SIGCONT]   = 18,
 	[SIGSTOP]   = 19,
 	[SIGTSTP]   = 20,
+#endif
 	[SIGTTIN]   = 21,
 	[SIGTTOU]   = 22,
+#if defined(__HyperbolaBSD__) || defined(__OpenBSD__)
+#if defined(SIGPOLL)
+	[SIGPOLL]   = 23,
+#endif
+#elif defined(__linux__)
 	[SIGURG]    = 23,
+#endif
 	[SIGXCPU]   = 24,
 	[SIGXFSZ]   = 25,
 	[SIGVTALRM] = 26,
 	[SIGPROF]   = 27,
+#if defined(SIGWINCH) || defined(__linux__)
 	[SIGWINCH]  = 28,
+#endif
+#if defined(__HyperbolaBSD__) || defined(__OpenBSD__)
+#if defined(SIGINFO)
+	[SIGINFO]   = 29,
+#endif
+	[SIGUSR1]   = 30,
+#if defined(SIGTHR)
+	[SIGUSR2]   = 31,
+	[SIGTHR]    = 32
+#else
+	[SIGUSR2]   = 31
+#endif
+#elif defined(__linux__)
 	[SIGPOLL]   = 29,
 	[SIGPWR]    = 30,
 	[SIGSYS]    = 31
+#endif
 };
 
 #define sigmap(x) ((x) >= sizeof map ? (x) : map[(x)])
@@ -65,15 +132,38 @@ static const char strings[] =
 	"Illegal instruction\0"
 	"Trace/breakpoint trap\0"
 	"Aborted\0"
+#if defined(__HyperbolaBSD__) || defined(__OpenBSD__)
+#if defined(SIGEMT)
+	"Emulator trap\0"
+#else
+	"Unknown signal\0"
+#endif
+#elif defined(__linux__)
 	"Bus error\0"
+#endif
 	"Arithmetic exception\0"
 	"Killed\0"
+#if defined(__HyperbolaBSD__) || defined(__OpenBSD__)
+	"Bus error\0"
+#elif defined(__linux__)
 	"User defined signal 1\0"
+#endif
 	"Segmentation fault\0"
+#if defined(__HyperbolaBSD__) || defined(__OpenBSD__)
+	"Bad system call\0"
+#elif defined(__linux__)
 	"User defined signal 2\0"
+#endif
 	"Broken pipe\0"
 	"Alarm clock\0"
 	"Terminated\0"
+#if defined(__HyperbolaBSD__) || defined(__OpenBSD__)
+	"Urgent I/O condition\0"
+	"Stopped (signal)\0"
+	"Stopped\0"
+	"Continued\0"
+	"Child process status\0"
+#elif defined(__linux__)
 #if defined(SIGSTKFLT)
 	"Stack fault\0"
 #elif defined(SIGEMT)
@@ -85,14 +175,37 @@ static const char strings[] =
 	"Continued\0"
 	"Stopped (signal)\0"
 	"Stopped\0"
+#endif
 	"Stopped (tty input)\0"
 	"Stopped (tty output)\0"
+#if defined(__HyperbolaBSD__) || defined(__OpenBSD__)
+	"I/O possible\0"
+#elif defined(__linux__)
 	"Urgent I/O condition\0"
+#endif
 	"CPU time limit exceeded\0"
 	"File size limit exceeded\0"
 	"Virtual timer expired\0"
 	"Profiling timer expired\0"
+#if defined(SIGWINCH) || defined(__linux__)
 	"Window changed\0"
+#else
+	"Unknown signal\0"
+#endif
+#if defined(__HyperbolaBSD__) || defined(__OpenBSD__)
+#if defined(SIGINFO)
+	"Information request\0"
+#else
+	"Unknown signal\0"
+#endif
+	"User defined signal 1\0"
+	"User defined signal 2\0"
+#if defined(SIGTHR)
+	"Thread AST"
+#else
+	"Unknown signal"
+#endif
+#elif defined(__linux__)
 	"I/O possible\0"
 	"Power failure\0"
 	"Bad system call\0"
@@ -110,6 +223,7 @@ static const char strings[] =
 	"\0RT105\0RT106\0RT107\0RT108\0RT109\0RT110\0RT111\0RT112"
 	"\0RT113\0RT114\0RT115\0RT116\0RT117\0RT118\0RT119\0RT120"
 	"\0RT121\0RT122\0RT123\0RT124\0RT125\0RT126\0RT127\0RT128"
+#endif
 #endif
 	"";
 
