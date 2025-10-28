4ad968ffca8fdd542e4ae0c85410ce26d26b9d97
Marcio Silva <coadde@hyperbola.info>
2025-10-28 20:26:47 -0300

    endian: fix byte swaps and add BSD swap macros
    - Correct __bswap32 and __bswap64 implementations to ensure proper
      bit masking and casting.
    - Add swap16, swap32, swap64 macros under _BSD_SOURCE for BSD
      compatibility
    
    Signed-off-by: Marcio Silva <coadde@hyperbola.info>

--- include/endian.h
+++ include/endian.h
@@ -23,13 +23,19 @@ static __inline uint16_t __bswap16(uint16_t __x)
 
 static __inline uint32_t __bswap32(uint32_t __x)
 {
-	return __x>>24 | __x>>8&0xff00 | __x<<8&0xff0000 | __x<<24;
+	return __x>>24 | (__x>>8&0xff00) | (__x<<8&0xff0000) | (__x<<24&0xff000000);
 }
 
 static __inline uint64_t __bswap64(uint64_t __x)
 {
-	return __bswap32(__x)+0ULL<<32 | __bswap32(__x>>32);
+	return (uint64_t)__bswap32(__x)<<32 | (uint64_t)__bswap32(__x>>32);
 }
+
+#ifdef _BSD_SOURCE
+#define swap16(x) __bswap16(x)
+#define swap32(x) __bswap32(x)
+#define swap64(x) __bswap64(x)
+#endif
 
 #if __BYTE_ORDER == __LITTLE_ENDIAN
 #define htobe16(x) __bswap16(x)
