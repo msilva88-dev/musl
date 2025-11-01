5bf9d417b644347ff86143519796d6b8dcecc4c9
Marcio Silva <coadde@hyperbola.info>
2025-11-01 13:16:48 -0300

    errc: add BSD errc and warnc support
    - Added src/legacy/errc.c with implementations
      of errc, verrc, warnc, and vwarnc.
    - Updated include/err.h with function declarations under _BSD_SOURCE.
    - Provides BSD-compatible error reporting with explicit error codes.
    
    Signed-off-by: Marcio Silva <coadde@hyperbola.info>

--- include/err.h
+++ include/err.h
@@ -12,11 +12,19 @@ void warn(const char *, ...);
 void vwarn(const char *, va_list);
 void warnx(const char *, ...);
 void vwarnx(const char *, va_list);
+#ifdef _BSD_SOURCE
+void warnc(int, const char *, ...);
+void vwarnc(int, const char *, va_list);
+#endif
 
 _Noreturn void err(int, const char *, ...);
 _Noreturn void verr(int, const char *, va_list);
 _Noreturn void errx(int, const char *, ...);
 _Noreturn void verrx(int, const char *, va_list);
+#ifdef _BSD_SOURCE
+_Noreturn void errc(int, int, const char *, ...);
+_Noreturn void verrc(int, int, const char *, va_list);
+#endif
 
 #ifdef __cplusplus
 }
---
+++ src/legacy/errc.c
@@ -0,0 +1,40 @@
+#define _BSD_SOURCE
+#include <err.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+
+extern char *__progname;
+
+void vwarnc(int code, const char *fmt, va_list ap)
+{
+	fprintf (stderr, "%s: ", __progname);
+	if (fmt) {
+		vfprintf(stderr, fmt, ap);
+		fputs (": ", stderr);
+	}
+	fputs(strerror(code), stderr);
+	fputc('\n', stderr);
+}
+
+_Noreturn void verrc(int status, int code, const char *fmt, va_list ap)
+{
+	vwarnc(code, fmt, ap);
+	exit(status);
+}
+
+void warnc(int code, const char *fmt, ...)
+{
+	va_list ap;
+	va_start(ap, fmt);
+	vwarnc(code, fmt, ap);
+	va_end(ap);
+}
+
+_Noreturn void errc(int status, int code, const char *fmt, ...)
+{
+	va_list ap;
+	va_start(ap, fmt);
+	verrc(status, code, fmt, ap);
+	va_end(ap);
+}
