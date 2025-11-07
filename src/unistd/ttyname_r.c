f17f46d1ad116a00eb68615b7b600bb2d7d31f29
Marcio Silva <coadde@hyperbola.info>
2025-11-07 16:39:23 -0300

    ttyname_r: support BSD variants for ttyname_r
    - Add checks for HyperbolaBSD and OpenBSD to use devname() for TTY.
    - Validate that fd is a character device and copy name safely.
    - Keep Linux implementation unchanged, conditional compilation only.
    
    Signed-off-by: Marcio Silva <coadde@hyperbola.info>

--- src/unistd/ttyname_r.c
+++ src/unistd/ttyname_r.c
@@ -1,10 +1,33 @@
 #include <unistd.h>
 #include <errno.h>
 #include <sys/stat.h>
+#if defined(__HyperbolaBSD__) || defined(__OpenBSD__)
+#include <string.h>
+#include <limits.h>
+#include <stdlib.h> // devname()
+#elif defined(__linux__)
 #include "syscall.h"
+#endif
 
 int ttyname_r(int fd, char *name, size_t size)
 {
+#if defined(__HyperbolaBSD__) || defined(__OpenBSD__)
+	struct stat st2;
+	size_t l;
+
+	if (!isatty(fd)) return errno;
+
+	if (fstat(fd, &st2)) return errno;
+	if (!S_ISCHR(st2.st_mode)) return ENOTTY;
+
+	const char *dname = devname(st2.st_rdev, S_IFCHR);
+	if (!dname) return ENOTTY;
+
+	l = strlen(dname) + 1;
+	if (l > size) return ERANGE;
+
+	memcpy(name, dname, l);
+#elif defined(__linux__)
 	struct stat st1, st2;
 	char procname[sizeof "/proc/self/fd/" + 3*sizeof(int) + 2];
 	ssize_t l;
@@ -24,5 +47,6 @@ int ttyname_r(int fd, char *name, size_t size)
 	if (st1.st_dev != st2.st_dev || st1.st_ino != st2.st_ino)
 		return ENODEV;
 
+#endif
 	return 0;
 }
