35362d3c2c1b263704da00e7d281a2804394c2e7
Marcio Silva <coadde@hyperbola.info>
2025-11-08 17:13:16 -0300

    link_ntoa: import BSD link-layer addr. formatter
    - Add link_ntoa() implementation from OpenBSD 7.0 libc.
    - Provide sockaddr_dl definition and LLADDR() macro in <net/if_dl.h>.
    - Supports conversion of AF_LINK addresses to printable form.
    - Update COPYRIGHT with BSD-3-Clause attribution.
    
    Signed-off-by: Marcio Silva <coadde@hyperbola.info>

---
+++ include/net/if_dl.h
@@ -0,0 +1,24 @@
+#ifndef _NET_IF_DL_H
+#define _NET_IF_DL_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define _SDL_DATA_LEN 24
+struct sockaddr_dl {
+	unsigned char sdl_len, sdl_family;
+	uint16_t sdl_index;
+        unsigned char sdl_type, sdl_nlen, sdl_alen, sdl_slen;
+        char sdl_data[_SDL_DATA_LEN];
+};
+
+#define LLADDR(sdl) (caddr_t)((sdl)->sdl_nlen + (sdl)->sdl_data)
+
+char *link_ntoa(const struct sockaddr_dl *);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
