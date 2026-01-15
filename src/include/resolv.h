71bf8fce6ee1a31afd0c083e49feedd42664e75d
Marcio Silva <coadde@hyperbola.info>
2026-01-15 16:17:17 -0300

    res_comp: remove duplicates provided by musl, comment unused helpers
    - Removed (`dn_expand`, `dn_comp`,
      and related compression/skipping routines) from the resolver header
      and module because they are already implemented in musl libc
      and do not need local exposure or duplication.
    - Commented out internal prototypes for unused DNS name helpers
      (`res_ownok`, `res_mailok`, `res_dnok`)
      and byte encoding routines (`__putlong`, `__putshort`)
      in the resolver header:
      * These are not currently implemented as they are not needed
        or used in the codebase.
      * Left commented as placeholders
        for future implementation if required.
    - Kept and exposed BSD-specific helpers
      (`__res_hnok`, `res_hnok`, `_getshort`, `_getlong`)
      for portability where musl does not provide equivalents.
    
    This update avoids code duplication with musl,
    clarifies what is actively implemented, and maintains minimal,
    purposeful exposure in the resolver API.
    
    Signed-off-by: Marcio Silva <coadde@hyperbola.info>

--- src/include/resolv.h
+++ src/include/resolv.h
@@ -9,4 +9,19 @@ hidden int __res_mkquery(int, const char *, int, int, const unsigned char *, int
 hidden int __res_send(const unsigned char *, int, unsigned char *, int);
 hidden int __res_msend(int, const unsigned char *const *, const int *, unsigned char *const *, int *, int);
 
+hidden int __res_hnok(const char *);
+hidden int res_hnok(const char *);
+/*
+hidden int res_dnok(const char *);
+hidden int res_ownok(const char *);
+hidden int res_mailok(const char *);
+*/
+
+hidden u_int16_t _getshort(const unsigned char *);
+hidden u_int32_t _getlong(const unsigned char *);
+/*
+hidden void __putlong(u_int32_t, unsigned char *);
+hidden void __putshort(u_int16_t, unsigned char *);
+*/
+
 #endif
--- src/network/res_comp.c
+++ src/network/res_comp.c
@@ -62,263 +62,6 @@
 #include <limits.h>
 #include <string.h>
 
-static int dn_find(u_char *, u_char *, u_char **, u_char **);
-
-/*
- * Expand compressed domain name 'comp_dn' to full domain name.
- * 'msg' is a pointer to the beginning of the message,
- * 'eomorig' points to the first location after the message,
- * 'exp_dn' is a pointer to a buffer of size 'length' for the result.
- * Return size of compressed name or -1 if there was an error.
- */
-int
-dn_expand(const u_char *msg, const u_char *eomorig, const u_char *comp_dn,
-    char *exp_dn, int length)
-{
-	const u_char *cp;
-	char *dn;
-	int n, c;
-	char *eom;
-	int len = -1, checked = 0;
-
-	dn = exp_dn;
-	cp = comp_dn;
-	if (length > HOST_NAME_MAX)
-		length = HOST_NAME_MAX;
-	eom = exp_dn + length;
-	/*
-	 * fetch next label in domain name
-	 */
-	while ((n = *cp++)) {
-		/*
-		 * Check for indirection
-		 */
-		switch (n & INDIR_MASK) {
-		case 0:
-			if (dn != exp_dn) {
-				if (dn >= eom)
-					return (-1);
-				*dn++ = '.';
-			}
-			if (dn+n >= eom)
-				return (-1);
-			checked += n + 1;
-			while (--n >= 0) {
-				if (((c = *cp++) == '.') || (c == '\\')) {
-					if (dn + n + 2 >= eom)
-						return (-1);
-					*dn++ = '\\';
-				}
-				*dn++ = c;
-				if (cp >= eomorig)	/* out of range */
-					return (-1);
-			}
-			break;
-
-		case INDIR_MASK:
-			if (len < 0)
-				len = cp - comp_dn + 1;
-			cp = msg + (((n & 0x3f) << 8) | (*cp & 0xff));
-			if (cp < msg || cp >= eomorig)	/* out of range */
-				return (-1);
-			checked += 2;
-			/*
-			 * Check for loops in the compressed name;
-			 * if we've looked at the whole message,
-			 * there must be a loop.
-			 */
-			if (checked >= eomorig - msg)
-				return (-1);
-			break;
-
-		default:
-			return (-1);			/* flag error */
-		}
-	}
-	*dn = '\0';
-	if (len < 0)
-		len = cp - comp_dn;
-	return (len);
-}
-DEF_WEAK(dn_expand);
-
-/*
- * Compress domain name 'exp_dn' into 'comp_dn'.
- * Return the size of the compressed name or -1.
- * 'length' is the size of the array pointed to by 'comp_dn'.
- * 'dnptrs' is a list of pointers to previous compressed names. dnptrs[0]
- * is a pointer to the beginning of the message. The list ends with NULL.
- * 'lastdnptr' is a pointer to the end of the arrary pointed to
- * by 'dnptrs'. Side effect is to update the list of pointers for
- * labels inserted into the message as we compress the name.
- * If 'dnptr' is NULL, we don't try to compress names. If 'lastdnptr'
- * is NULL, we don't update the list.
- */
-int
-dn_comp(const char *exp_dn, u_char *comp_dn, int length, u_char **dnptrs,
-    u_char **lastdnptr)
-{
-	u_char *cp, *dn;
-	int c, l;
-	u_char **cpp, **lpp, *sp, *eob;
-	u_char *msg;
-
-	dn = (u_char *)exp_dn;
-	cp = comp_dn;
-	eob = cp + length;
-	lpp = cpp = NULL;
-	if (dnptrs != NULL) {
-		if ((msg = *dnptrs++) != NULL) {
-			for (cpp = dnptrs; *cpp != NULL; cpp++)
-				;
-			lpp = cpp;	/* end of list to search */
-		}
-	} else
-		msg = NULL;
-	for (c = *dn++; c != '\0'; ) {
-		/* look to see if we can use pointers */
-		if (msg != NULL) {
-			if ((l = dn_find(dn-1, msg, dnptrs, lpp)) >= 0) {
-				if (cp+1 >= eob)
-					return (-1);
-				*cp++ = (l >> 8) | INDIR_MASK;
-				*cp++ = l % 256;
-				return (cp - comp_dn);
-			}
-			/* not found, save it */
-			if (lastdnptr != NULL && cpp < lastdnptr-1) {
-				*cpp++ = cp;
-				*cpp = NULL;
-			}
-		}
-		sp = cp++;	/* save ptr to length byte */
-		do {
-			if (c == '.') {
-				c = *dn++;
-				break;
-			}
-			if (c == '\\') {
-				if ((c = *dn++) == '\0')
-					break;
-			}
-			if (cp >= eob) {
-				if (msg != NULL)
-					*lpp = NULL;
-				return (-1);
-			}
-			*cp++ = c;
-		} while ((c = *dn++) != '\0');
-		/* catch trailing '.'s but not '..' */
-		if ((l = cp - sp - 1) == 0 && c == '\0') {
-			cp--;
-			break;
-		}
-		if (l <= 0 || l > MAXLABEL) {
-			if (msg != NULL)
-				*lpp = NULL;
-			return (-1);
-		}
-		*sp = l;
-	}
-	if (cp >= eob) {
-		if (msg != NULL)
-			*lpp = NULL;
-		return (-1);
-	}
-	*cp++ = '\0';
-	return (cp - comp_dn);
-}
-
-/*
- * Skip over a compressed domain name. Return the size or -1.
- */
-int
-__dn_skipname(const u_char *comp_dn, const u_char *eom)
-{
-	const u_char *cp;
-	int n;
-
-	cp = comp_dn;
-	while (cp < eom && (n = *cp++)) {
-		/*
-		 * check for indirection
-		 */
-		switch (n & INDIR_MASK) {
-		case 0:			/* normal case, n == len */
-			cp += n;
-			continue;
-		case INDIR_MASK:	/* indirection */
-			cp++;
-			break;
-		default:		/* illegal type */
-			return (-1);
-		}
-		break;
-	}
-	if (cp > eom)
-		return (-1);
-	return (cp - comp_dn);
-}
-
-static int
-mklower(int ch)
-{
-	if (isascii(ch) && isupper(ch))
-		return (tolower(ch));
-	return (ch);
-}
-
-/*
- * Search for expanded name from a list of previously compressed names.
- * Return the offset from msg if found or -1.
- * dnptrs is the pointer to the first name on the list,
- * not the pointer to the start of the message.
- */
-static int
-dn_find(u_char *exp_dn, u_char *msg, u_char **dnptrs, u_char **lastdnptr)
-{
-	u_char *dn, *cp, **cpp;
-	int n;
-	u_char *sp;
-
-	for (cpp = dnptrs; cpp < lastdnptr; cpp++) {
-		dn = exp_dn;
-		sp = cp = *cpp;
-		while ((n = *cp++)) {
-			/*
-			 * check for indirection
-			 */
-			switch (n & INDIR_MASK) {
-			case 0:		/* normal case, n == len */
-				while (--n >= 0) {
-					if (*dn == '.')
-						goto next;
-					if (*dn == '\\')
-						dn++;
-					if (mklower(*dn++) != mklower(*cp++))
-						goto next;
-				}
-				if ((n = *dn++) == '\0' && *cp == '\0')
-					return (sp - msg);
-				if (n == '.')
-					continue;
-				goto next;
-
-			case INDIR_MASK:	/* indirection */
-				cp = msg + (((n & 0x3f) << 8) | *cp);
-				break;
-
-			default:	/* illegal type */
-				return (-1);
-			}
-		}
-		if (*dn == '\0')
-			return (sp - msg);
-	next:	;
-	}
-	return (-1);
-}
-
 /*
  * Verify that a domain name uses an acceptable character set.
  */
@@ -343,8 +86,7 @@ dn_find(u_char *exp_dn, u_char *msg, u_char **dnptrs, u_char **lastdnptr)
 #define middlechar(c) (borderchar(c) || hyphenchar(c) || underscorechar(c))
 #define	domainchar(c) ((c) > 0x20 && (c) < 0x7f)
 
-int
-__res_hnok(const char *dn)
+int __res_hnok(const char *dn)
 {
 	int pch = PERIOD, ch = *dn++;
 
@@ -355,53 +97,54 @@ __res_hnok(const char *dn)
 			;
 		} else if (periodchar(pch)) {
 			if (!borderchar(ch))
-				return (0);
+				return 0;
 		} else if (periodchar(nch) || nch == '\0') {
 			if (!borderchar(ch))
-				return (0);
+				return 0;
 		} else {
 			if (!middlechar(ch))
-				return (0);
+				return 0;
 		}
 		pch = ch, ch = nch;
 	}
-	return (1);
+	return 1;
 }
-DEF_STRONG(__res_hnok);
+strong_alias(__res_hnok, res_hnok);
 
 /*
  * hostname-like (A, MX, WKS) owners can have "*" as their first label
  * but must otherwise be as a host name.
  */
-int
-res_ownok(const char *dn)
+/*
+int res_ownok(const char *dn)
 {
 	if (asterchar(dn[0])) {
 		if (periodchar(dn[1]))
-			return (res_hnok(dn+2));
+			return res_hnok(dn+2);
 		if (dn[1] == '\0')
-			return (1);
+			return 1;
 	}
-	return (res_hnok(dn));
+	return res_hnok(dn);
 }
+*/
 
 /*
  * SOA RNAMEs and RP RNAMEs can have any printable character in their first
  * label, but the rest of the name has to look like a host name.
  */
-int
-res_mailok(const char *dn)
+/*
+int res_mailok(const char *dn)
 {
 	int ch, escaped = 0;
-
+*/
 	/* "." is a valid missing representation */
-	if (*dn == '\0')
-		return(1);
-
+/*	if (*dn == '\0')
+		return 1;
+*/
 	/* otherwise <label>.<hostname> */
-	while ((ch = *dn++) != '\0') {
+/*	while ((ch = *dn++) != '\0') {
 		if (!domainchar(ch))
-			return (0);
+			return 0;
 		if (!escaped && periodchar(ch))
 			break;
 		if (escaped)
@@ -410,57 +153,57 @@ res_mailok(const char *dn)
 			escaped = 1;
 	}
 	if (periodchar(ch))
-		return (res_hnok(dn));
-	return(0);
+		return res_hnok(dn);
+	return 0;
 }
+*/
 
 /*
  * This function is quite liberal, since RFC 1034's character sets are only
  * recommendations.
  */
-int
-res_dnok(const char *dn)
+/*
+int res_dnok(const char *dn)
 {
 	int ch;
 
 	while ((ch = *dn++) != '\0')
 		if (!domainchar(ch))
-			return (0);
-	return (1);
+			return 0;
+	return 1;
 }
+*/
 
 /*
  * Routines to insert/extract short/long's.
  */
 
-u_int16_t
-_getshort(const u_char *msgp)
+u_int16_t ___getshort(const u_char *msgp)
 {
 	u_int16_t u;
 
 	GETSHORT(u, msgp);
-	return (u);
+	return u;
 }
-DEF_STRONG(_getshort);
+strong_alias(___getshort, _getshort);
 
-u_int32_t
-_getlong(const u_char *msgp)
+u_int32_t ___getlong(const u_char *msgp)
 {
 	u_int32_t u;
 
 	GETLONG(u, msgp);
-	return (u);
+	return u;
 }
-DEF_STRONG(_getlong);
+strong_alias(___getlong, _getlong);
 
-void
-__putshort(u_int16_t s, u_char *msgp)
+/*
+void __putshort(u_int16_t s, u_char *msgp)
 {
 	PUTSHORT(s, msgp);
 }
 
-void
-__putlong(u_int32_t l, u_char *msgp)
+void __putlong(u_int32_t l, u_char *msgp)
 {
 	PUTLONG(l, msgp);
 }
+*/
